ca3d5524eb6af740b0cfd707ff8333959571b7a8
Yuichi NAGAKURA <naruhodo.u1az@gmail.com>
2020-08-10 18:50:05 +0900

    first commit

---
+++ src/AwsCdkPhoneVertifyApi/Function.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace AwsCdkPhoneVertifyApi
+{
+    public class Functions
+    {
+        public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+
+            System.Console.WriteLine($"Request: {request.Body}");
+
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.OK,
+                Body = "Hello Dotnet Lambda",
+            };
+
+        }
+    }
+}
